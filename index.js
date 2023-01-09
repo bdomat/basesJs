7e7159a0d2a4167bbec30e1911356b0eefc3084b
Bertrand Domat <bertrand.domat@gmail.com>
2023-01-09 16:25:27 +0100

    initialisation

---
+++ index.js
@@ -0,0 +1,33 @@
+// alert("Salut Js");
+
+// console.log("Coucou");
+
+// Kamel Case
+let maSuperVariable = "Hello";
+
+// ** Les variables **
+// var = Vieu JS
+var unTexte = "voici un texte";
+
+console.log(unTexte);
+
+// const = constante
+const prenom = "Justine";
+
+// Let = la variable peut évoluer
+let unChiffre = 24;
+unChiffre = 22;
+console.log(unChiffre);
+
+let Chaine = "Je suis une chaine de caractères";
+
+let nouvelleChaine = "Chaine précédente : " + Chaine + ". Au top !";
+
+console.log(nouvelleChaine);
+
+// Concatenation avec guillemets altgr+7
+let autreConcatenation = `Chaines précédente ${Chaine}. Au top !`;
+
+//** Types de données **
+let string = "Je suis une chaine de caractère";
+let number = 14;
